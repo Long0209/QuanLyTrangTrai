8fdf99cac86772d8f253f3544861f7a6802a8b4a
Long0209 <lookbook135@gmail.com>
2023-11-20 00:43:33 +0700

    new file icons and imagea

---
+++ Client/src/Admin/Components/index.js
@@ -0,0 +1,7 @@
+
+// Layout
+export { default as AdminLayout } from './AdminLayout/AdminLayout.jsx'; 
+
+// List
+export { default as FarmList } from './ListAdmin/FarmList.jsx'; 
+export { default as ListAccount } from './ListAdmin/ListAccount.jsx'; 
