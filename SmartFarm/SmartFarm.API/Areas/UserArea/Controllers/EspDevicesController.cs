7dd1482260c23061935f551137be417c3f1c42cb
Quoc Tuan <quoctuan121101@gmail.com>
2023-12-06 21:24:39 +0700

    add espdevice api

--- SmartFarm/SmartFarm.API/Areas/UserArea/Controllers/EspDevicesController.cs
+++ SmartFarm/SmartFarm.API/Areas/UserArea/Controllers/EspDevicesController.cs
@@ -1,32 +1,30 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SmartFarm.Data;
-using Microsoft.AspNetCore.Identity;
-using SmartFarm.API.Models;
-using SmartFarm.API.Extensions.PagedList.Models;
 using SmartFarm.API.Extensions.PagedList;
+using SmartFarm.API.Extensions.PagedList.Models;
+using SmartFarm.Data;
 
-namespace SmartFarm.API.UserArea.Controllers;
+namespace SmartFarm.API.Areas.UserArea.Controllers;
 
-// API to access espdata
-[ApiController]
-[Route("api/user/[controller]")]
 [Authorize]
-public class EspDataController : ControllerBase {
+[ApiController]
+[Area("user")]
+[Route("api/[area]/[controller]")]
+public class EspDevicesController : ControllerBase {
     private readonly SmartFarmDbContext _dbContext;
-    public EspDataController(
+    public EspDevicesController(
         SmartFarmDbContext context) {
             _dbContext = context;
     }
 
     /// <summary>
-    /// Retrieves information about ESP devices owned by the authenticated user
+    /// Retrieves information about ESP devices owned by logged in user
     /// </summary>
-    /// <returns>List of esp owned by the user</returns>
+    /// <returns>List of esp owned by the logged in user</returns>
     [HttpGet]
     [Route("get-esp-device-owner")]
-    public IActionResult GetEspDeviceOwner([FromBody] PageInputModel pageInputModel) {
+    public IActionResult GetEspDeviceOwner([FromQuery] PageInputModel pageInputModel) {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
         var dataSource = _dbContext.EspDevices
@@ -46,7 +44,7 @@ public class EspDataController : ControllerBase {
     [HttpGet]
     [Route("get-esp-data")]
     [Authorize]
-    public IActionResult GetEspData(int deviceId, int? resultSize) {
+    public IActionResult GetEspData(int deviceId, int resultSize = 200) {
         // Obtain the user ID from the authenticated user's claims
         var userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
 
@@ -60,13 +58,10 @@ public class EspDataController : ControllerBase {
             return BadRequest(new { message = "Device not found or user doesn't own the device." });
         }
 
-        // Set a default result size of 500 if not specified or negative
-        int _resultSize = resultSize.HasValue && resultSize > 0 ? resultSize.Value : 500;
-
         // Retrieve the last 1000 records of ESP data for the specified device
         var result = _dbContext.EspData
             .Where(m => m.DeviceId == deviceId)
-            .Take(_resultSize)
+            .Take(resultSize)
             .ToList();
 
         // Return the ESP data in the HTTP response
