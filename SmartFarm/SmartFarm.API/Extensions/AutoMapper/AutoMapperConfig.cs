ca709bc58ecc8f8440a31d383647aa20238adb84
Quoc Tuan <quoctuan121101@gmail.com>
2023-12-06 12:25:31 +0700

    Add automapper

---
+++ SmartFarm/SmartFarm.API/Extensions/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using SmartFarm.API.Models.EspDevices;
+using SmartFarm.API.Models.Identity;
+using SmartFarm.Data.Entities;
+
+namespace SmartFarm.API.Extensions.AutoMapper;
+
+public class AutoMapperConfig : Profile {
+    public void AutoMapperProfile() {
+        CreateMap<SignUpViewModel, User>();
+        CreateMap<AddEspDeviceModel, EspDevice>();
+    }
+}
