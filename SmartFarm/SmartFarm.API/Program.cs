f23527e27e73b57e8d9e1f836c0c51e73f23a004
Quoc Tuan <quoctuan121101@gmail.com>
2023-12-06 20:23:02 +0700

    Enable code summary for swagegr ui

--- SmartFarm/SmartFarm.API/Program.cs
+++ SmartFarm/SmartFarm.API/Program.cs
@@ -13,7 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "SmartFarm.API.xml"));
+});
 
 // Add database context to services
 builder.Services.AddDbContext<SmartFarmDbContext>(options => {
