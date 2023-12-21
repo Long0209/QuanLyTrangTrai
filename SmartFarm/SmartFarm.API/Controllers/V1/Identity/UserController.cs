77ace6d4266644e0c8c3db3b28e42ca5bca1364d
Quoc Tuan <quoctuan121101@gmail.com>
2023-12-21 21:03:46 +0700

    [fea] update password for user

---
+++ SmartFarm/SmartFarm.API/Controllers/V1/Identity/UserController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmartFarm.API.Models.Identity;
+using SmartFarm.Data.Entities;
+
+namespace SmartFarm.API.Controllers.V1.Identity;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/identity/user")]
+public class UserController : ControllerBase {
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    
+    public UserController(
+        UserManager<User> userManager,
+        SignInManager<User> signInManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+    }
+
+    /// <summary>
+    /// Handles HTTP PATCH requests to update the password for the authenticated user.
+    /// </summary>
+    /// <param name="updatePasswordViewModel">A model containing the old and new passwords.</param>
+    /// <returns>
+    /// - 200 OK: If the password is updated successfully.
+    /// - 400 Bad Request: If the ModelState is invalid or if there are errors during the password change.
+    /// - 404 Not Found: If the user associated with the authenticated identity is not found.
+    /// </returns>
+    [HttpPatch]
+    [Route("password")]
+    public async Task<IActionResult> UpdatePassword([FromBody] UpdatePasswordViewModel updatePasswordViewModel){
+        if (ModelState.IsValid) {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            if (user == null) {
+                return NotFound(new { Message = "User not found." });
+            }
+
+            var result = await _userManager.ChangePasswordAsync(user, updatePasswordViewModel.OldPassword, updatePasswordViewModel.NewPassword);
+
+            if (result.Succeeded){
+                return Ok(new { Message = "Password updated successfully." });
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        return BadRequest(ModelState);
+    }
+}
